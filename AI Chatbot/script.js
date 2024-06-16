0ab9cc13e3c0ae8b12f498e5b3a4b1f17870ab43
Alledanaralle <139613761+Alledanaralle@users.noreply.github.com>
2024-06-16 19:26:08 +0700

    Add files via upload

---
+++ AI Chatbot/script.js
@@ -0,0 +1,77 @@
+function openChatBot() {
+    document.body.classList.add('show-chatbot');
+}
+
+function closeChatBot() {
+    document.body.classList.remove('show-chatbot');
+}
+
+
+const chatInput = document.querySelector('#text-input'),
+    sendChatBtn = document.querySelector('#send-btn'),
+    chatbox = document.querySelector('.chatbox'),
+    API_KEY = "sk-proj-qE8WJxt78L38OnAMrXBqT3BlbkFJJd46YHMl3w3EFPRahCoh";
+
+let userMessage;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement('li');
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector('p').textContent = message;
+    return chatLi;
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions",
+        messageElement = incomingChatLi.querySelector('p'),
+        requestOptions = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: userMessage }]
+            }
+            )
+        };
+
+    fetch(API_URL, requestOptions).
+        then(res => res.json()).
+        then(data => {
+            if (data.error && data.error.message) {
+                throw new Error(data.error.message);
+            }
+            messageElement.textContent = data.choices[0].message.content;
+        }).catch((error) => {
+            console.error('Error fetching the API:', error);
+            messageElement.classList.add("error")
+            messageElement.textContent = "Oops! Couldn't get Sigma answer. Please try again.";
+        })
+}
+
+const scrollToBottom = () => {
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    console.log('User message:', userMessage);  // Debugging log
+    if (!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatInput.value = '';  // Clear the input after sending the message
+
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Waiting for Sigma answer...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        scrollToBottom();
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+sendChatBtn.addEventListener('click', handleChat);
+
